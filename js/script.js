cab9b201b85f3afbca119a753416a8128490e9b4
ludniz <69349076+ludniz@users.noreply.github.com>
2022-06-23 21:23:11 +0200

    Updated shop page
    started with filtering

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// init Isotope
-var $grid = $('#product-list').isotope({
-    // options
-  });
-  // filter items on button click
-  $('.filter-button-group').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-  });
